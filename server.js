dd376793d1f4866c23398d25523e3fcc42ffb0f6
Aastha sarswat <aasthasarswat24@gmail.com>
2021-02-01 18:51:53 +0530

    make home page

--- server.js
+++ server.js
@@ -3,18 +3,24 @@ const app=express();
 const ejs =require('ejs');
 const path=require('path');
 const expressLayout =require("express-ejs-layouts"); 
+var favicon = require('serve-favicon')
 
 const PORT=process.env.PORT||9000;
 
+//assets
+app.use(express.static('public'));
+app.get('/',(req,res)=>{
+    res.render("home");
+}); 
+//favicon
+app.use(favicon(path.join(__dirname, '/public/favicon.ico')));
 //set Template Engine
 app.use(expressLayout);
 app.set("views", path.join(__dirname, "/resources/views/"));
 
+
 app.set('view engine', 'ejs'); 
 
-app.get('/',(req,res)=>{
-    res.render('home');
-}); 
 
 app.listen(PORT,()=>{
     console.log(`listening on port ${PORT}`)
