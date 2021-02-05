11ae76ebe2bc1e1e1b8a9489f97c16ea81d41a33
Aastha sarswat <aasthasarswat24@gmail.com>
2021-02-05 09:40:48 +0530

    add to cart

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,33 @@
-console.log("hello");
+const axios=require('axios');
+const Noty=require('noty');
+let addToCart= document.querySelectorAll('.addto-cart')
+let cartCounter=document.querySelector('#cartCounter')
+
+function updateCart(brownie){
+    //post request
+    axios.post('/update-cart',brownie).then(res=>{
+        new Noty({
+            type:'success',
+            timeout:1000,
+            progressBar:false,
+            text:'item added to cart',
+        }).show();
+        cartCounter.innerText = res.data.totalQty
+    }).catch(err=>{
+        new Noty({
+            type:'error',
+            timeout:1000,
+            progressBar:false,
+            text:'something went wrong',
+        }).show();
+    })
+}
+
+
+addToCart.forEach((btn)=>{
+    btn.addEventListener('click',()=>{
+        let brownie =JSON.parse(btn.dataset.brownie)
+        updateCart(brownie)
+        console.log(brownie)
+    })
+})
--- server.js
+++ server.js
@@ -1,25 +1,66 @@
+require('dotenv').config()
 const express= require('express');
 const app=express();
 const ejs =require('ejs');
 const path=require('path');
 const expressLayout =require("express-ejs-layouts"); 
-var favicon = require('serve-favicon')
+require('mongodb');
+const mongoose= require("mongoose");
+const session= require("express-session");
+const flash=require("express-flash");
+const MongoDbStore=require("connect-mongo")(session);
+
 
 const PORT=process.env.PORT||9000;
 
-//assets
+//database connection
+const url='mongodb://localhost/brownies';
+mongoose.connect(url,{useNewUrlParser:true, useCreateIndex:true, useUnifiedTopology:true, useFindAndModify:true});
+const connection=mongoose.connection;
+connection.once('open',()=>{
+    console.log("Database connected.......");
+}).catch(err=>{
+    console.log("connection failed......")
+});
+
+
+//session store
+let mongoStore=new MongoDbStore({
+    mongooseConnection:connection,
+    collection:'sessions'
+})
+//session config............it works as a middleware
+app.use(session({
+    secret:process.env.COOKIE_SECRET,          //cookies
+    resave:false,
+    store:mongoStore,
+    saveUninitialized:false,
+     cookie:{maxAge:1000*60*60*24} //24hrs            //life time of cookie in mili sec
+}))
+
+app.use(flash());
+
+
 app.use(express.static('public'));
-app.get('/',(req,res)=>{
-    res.render("home");
-}); 
-//favicon
-app.use(favicon(path.join(__dirname, '/public/favicon.ico')));
+
+app.use(express.json())
+
+
+//global middleware
+app.use((req,res,next)=>{
+    res.locals.session = req.session
+    next()
+})
+
+
 //set Template Engine
 app.use(expressLayout);
 app.set("views", path.join(__dirname, "/resources/views/"));
 
 
 app.set('view engine', 'ejs'); 
+
+require('./routes/web')(app)
 
 
 app.listen(PORT,()=>{
