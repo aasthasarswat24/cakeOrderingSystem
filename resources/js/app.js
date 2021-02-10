d1ac0a71a2995837c7b1264483934e2e335361c0
Aastha sarswat <aasthasarswat24@gmail.com>
2021-02-10 11:22:28 +0530

    socket integration

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,33 +1,113 @@
-const axios=require('axios');
-const Noty=require('noty');
-let addToCart= document.querySelectorAll('.addto-cart')
-let cartCounter=document.querySelector('#cartCounter')
+import axios from 'axios'
+import Noty from 'noty'
+import  initAdmin from './admin'
+import moment from 'moment'
 
-function updateCart(brownie){
-    //post request
-    axios.post('/update-cart',brownie).then(res=>{
-        new Noty({
-            type:'success',
-            timeout:1000,
-            progressBar:false,
-            text:'item added to cart',
-        }).show();
-        cartCounter.innerText = res.data.totalQty
-    }).catch(err=>{
-        new Noty({
-            type:'error',
-            timeout:1000,
-            progressBar:false,
-            text:'something went wrong',
-        }).show();
-    })
+
+let addToCart = document.querySelectorAll('.add-to-cart')
+let cartCounter = document.querySelector('#cartCounter')
+
+function updateCart(brownie) {
+   axios.post('/update-cart', brownie).then(res => {
+       cartCounter.innerText = res.data.totalQty
+       new Noty({
+           type: 'success',
+           timeout: 1000,
+           text: 'Item added to cart',
+           progressBar: false,
+       }).show();
+   }).catch(err => {
+       new Noty({
+           type: 'error',
+           timeout: 1000,
+           text: 'Something went wrong',
+           progressBar: false,
+       }).show();
+   })
+}
+
+addToCart.forEach((btn) => {
+   btn.addEventListener('click', (e) => {
+       let brownie = JSON.parse(btn.dataset.brownie)
+       updateCart(brownie)
+   })
+})
+
+// Remove alert message after X seconds
+const alertMsg = document.querySelector('#success-alert')
+if(alertMsg) {
+   setTimeout(() => {
+       alertMsg.remove()
+   }, 2000)
 }
 
 
-addToCart.forEach((btn)=>{
-    btn.addEventListener('click',()=>{
-        let brownie =JSON.parse(btn.dataset.brownie)
-        updateCart(brownie)
-        console.log(brownie)
+
+
+//change order status
+let statuses = document.querySelectorAll('.status_line')
+let hiddenInput = document.querySelector('#hiddenInput')
+let order = hiddenInput ? hiddenInput.value : null
+order = JSON.parse(order)
+//let time = document.createElement('small');
+
+function updateStatus(order) {
+    statuses.forEach((status) => {
+        status.classList.remove('step-completed')
+        status.classList.remove('current')
     })
+    let stepCompleted = true;
+    //time.innerText = moment(order.updatedAt).format('hh:mm A')
+    //status.appendChild(time)
+
+    statuses.forEach((status) => {
+       let dataProp = status.dataset.status
+       if(stepCompleted) {
+            status.classList.add('step-completed')
+       }
+       if(dataProp === order.status) {
+            stepCompleted = false
+           if(status.nextElementSibling) {
+            status.nextElementSibling.classList.add('current')
+           }
+       }
+    })
+
+}
+
+updateStatus(order);
+
+
+
+
+//socket
+let socket = io()
+//join
+if(order) {
+    socket.emit('join', `order_${order._id}`)
+//order_1233(id) gives a unique room for every order
+}
+
+
+let adminAreaPath = window.location.pathname
+console.log(adminAreaPath)
+if(adminAreaPath.includes('admin'))
+initAdmin(socket)
+ {
+     socket.emit('join', 'adminRoom')
+ }
+
+socket.on('orderUpdated', () => {
+    const updatedOrder = {...order}
+    updatedOrder.updatedAt = moment().format()
+    updatedOrder.status = data.status
+    updateStatus(updatedOrder)
+    new Noty({
+        type:'success',
+        timeout:1000,
+        text:'order updated',
+        progressBar:false
+    }).show();
+    console.log(data)
 })
+
--- server.js
+++ server.js
@@ -10,6 +10,7 @@ const session= require("express-session");
 const flash=require("express-flash");
 const passport = require('passport');
 const MongoDbStore=require("connect-mongo")(session);
+const Emitter = require('events')
 
 
 const PORT=process.env.PORT||9000;
@@ -31,6 +32,11 @@ let mongoStore=new MongoDbStore({
     mongooseConnection:connection,
     collection:'sessions'
 })
+
+//emitter
+const eventEmitter = new Emitter()
+app.set('eventEmitter', eventEmitter)
+
 //session config............it works as a middleware
 app.use(session({
     secret:process.env.COOKIE_SECRET,          //cookies
@@ -78,6 +84,22 @@ app.set('view engine', 'ejs');
 require('./routes/web')(app)
 
 
-app.listen(PORT,()=>{
+const server = app.listen(PORT,()=>{
     console.log(`listening on port ${PORT}`)
 });
+
+//socket
+const io = require('socket.io')(server)
+io.on('connection' , (socket) => {
+    //join 
+    socket.on('join', (orderId) => {
+           socket.join(orderId)
+    })
+})
+
+eventEmitter.on('orderUpdated', (data)=>{
+    io.to(`order_${data.id}`).emit('orderUpdated',data)
+})
+eventEmitter.on('orderPlaced', (data)=>{
+    io.to('adminRoom').emit('orderUpdated',data)
+})
